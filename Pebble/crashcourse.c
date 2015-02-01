17ca7bb110e90f3a578a6249a25462031c0d75fe
Steven Jia <stevenjia@outlook.com>
2015-01-31 23:02:41 -0500

    Update crashcourse.c

--- Pebble/crashcourse.c
+++ Pebble/crashcourse.c
@@ -1,8 +1,9 @@
 #include <pebble.h>
-#define KEY_DATA 5
+#include <stdio.h>
+#include <stdlib.h>
 
-static Window *my_window;
-static TextLayer *time_layer, *address_text_layer, *distance_text_layer;
+static Window *main_window, *calibration_window;
+static TextLayer *output_layer, *timer_layer, *cardinal_text_layer, *distance_text_layer;
 //BitmapLayer *direction_arrow;
 static Layer *path_layer;
 
@@ -25,7 +26,6 @@ static int path_angle = 0;
 
 static void path_layer_update_callback(Layer *me, GContext *ctx) {
   (void)me;
-  // You can rotate the path before rendering
   gpath_rotate_to(current_path, (TRIG_MAX_ANGLE / 360) * path_angle);
   graphics_context_set_fill_color(ctx, GColorBlack);
   gpath_draw_filled(ctx, current_path);
@@ -34,6 +34,8 @@ static void path_layer_update_callback(Layer *me, GContext *ctx) {
 static int path_angle_add(int angle) {
   return path_angle = (path_angle + angle) % 360;
 }
+
+// Remove in final version *********************************************
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Rotate the path counter-clockwise
@@ -52,90 +54,119 @@ static void config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-//Time
+// ***********************************************************************
 
-static void update_time() {
-  
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  
-  static char buffer[] = "00:00";
-  
-  if(clock_is_24h_style() == true){
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+// App communication
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+
+  Tuple *t = dict_read_first(iterator);
+
+  while (t != NULL) {
+    
+    static char distance[64], timer[64], angle[64];
+    
+    switch (t->key) {
+      case 0:
+        snprintf(distance, sizeof(distance), "%s", t->value->cstring);
+        text_layer_set_text(distance_text_layer, distance);
+        break;
+      case 1:
+        snprintf(angle, sizeof(angle), "%s", t->value->cstring);
+// THIS NEEDS FIXING        
+        rotate_arrow(angle);
+        break;
+      case 5:
+        snprintf(timer, sizeof(timer), "%s", t->value->cstring);
+        text_layer_set_text(timer_layer, timer);
+        break;
+      case 6:
+        if(t->value->int8){
+          vibes_long_pulse();
+        }
+        break;
+    }
+    
+    t = dict_read_next(iterator);
+    
   }
+}
+
+void rotate_arrow(static int angle){
   
-  text_layer_set_text(time_layer, buffer);
+  if ((angle > 337.5 && angle <= 360) || (angle >= 0 && angle <= 22.5)){
+    path_angle = 0;
+    text_layer_set_text(cardinal_text_layer, "North");
+  } else if (angle > 22.5 && angle <= 67.5){
+    path_angle = 45;
+    text_layer_set_text(cardinal_text_layer, "Northwest");
+  } else if (angle > 67.5 && angle <= 112.5){
+    path_angle = 90;
+    text_layer_set_text(cardinal_text_layer, "East");
+  } else if (angle > 112.5 && angle <= 157.5){
+    path_angle = 135;
+    text_layer_set_text(cardinal_text_layer, "Southeast");
+  } else if (angle > 157.5 && angle <= 202.5){
+    path_angle = 180;
+    text_layer_set_text(cardinal_text_layer, "South");
+  } else if (angle > 202.5 && angle <= 247.5){
+    path_angle = 225;
+    text_layer_set_text(cardinal_text_layer, "Southwest");
+  } else if (angle > 247.5 && angle <= 292.5){
+    path_angle = 270;
+    text_layer_set_text(cardinal_text_layer, "West");
+  } else if (angle > 292.5 && angle <= 337.5){
+    path_angle = 315;
+    text_layer_set_text(cardinal_text_layer, "Northwest");
+  }
 }
-
-/* Button clicks
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up pressed!");
-}
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select pressed!");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down pressed!");
-}
-
-
-static void click_config_provider(void *context) {
-  // Register the ClickHandlers
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
- // window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
-
-}
-*/
 
 // Windows
+
+static void calibration_window_load(Window *window) {
+  
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+  
+}
+
+static void calibration_window_unload(Window *window) {
+  
+}
+
 
 static void main_window_load(Window *window) {
   
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
-  // Time
-  time_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h/2));
-  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text_color(time_layer, GColorBlack);
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-  text_layer_set_text(time_layer, "00:00");
-  layer_add_child(window_layer, text_layer_get_layer(time_layer));
+  // Timer
+  timer_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h/2));
+  text_layer_set_font(timer_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_color(timer_layer, GColorBlack);
+  text_layer_set_text_alignment(timer_layer, GTextAlignmentCenter);
+  text_layer_set_text(timer_layer, "00:00");
+  layer_add_child(window_layer, text_layer_get_layer(timer_layer));
   
-  // Address
+  // Cardinal directions
   
-  address_text_layer = text_layer_create(GRect(0, 104, window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_font(address_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-  text_layer_set_text_color(address_text_layer, GColorBlack);
-  text_layer_set_overflow_mode(address_text_layer, GTextOverflowModeFill);
-  text_layer_set_text_alignment(address_text_layer, GTextAlignmentCenter);
-  text_layer_set_text(address_text_layer, "123 University Ave.");
-  layer_add_child(window_layer, text_layer_get_layer(address_text_layer));
+  cardinal_text_layer = text_layer_create(GRect(0, 100, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_font(cardinal_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_color(cardinal_text_layer, GColorBlack);
+  text_layer_set_overflow_mode(cardinal_text_layer, GTextOverflowModeFill);
+  text_layer_set_text_alignment(cardinal_text_layer, GTextAlignmentCenter);
+  text_layer_set_text(cardinal_text_layer, "North");
+  layer_add_child(window_layer, text_layer_get_layer(cardinal_text_layer));
   
   // Distance
-                                 
+  
   distance_text_layer = text_layer_create(GRect(0, 120, window_bounds.size.w, window_bounds.size.h));
   text_layer_set_font(distance_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_color(distance_text_layer, GColorBlack);
   text_layer_set_overflow_mode(distance_text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(distance_text_layer, GTextAlignmentCenter);
-  text_layer_set_text(distance_text_layer, "43 metres");
+  text_layer_set_text(distance_text_layer, "0 metres");
   layer_add_child(window_layer, text_layer_get_layer(distance_text_layer));
-  
-  // Direction arrow                               
-  /*
-  direction_arrow = bitmap_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
-  bitmap_layer_set_alignment(direction_arrow, GAlignTop);
-  layer_add_child(window_layer, bitmap_layer_get_layer(direction_arrow));
-  */
   
   // Path
   
@@ -145,48 +176,53 @@ static void main_window_load(Window *window) {
   arrow_path = gpath_create(&ARROW_PATH_POINTS);
   current_path = arrow_path;
   gpath_move_to(current_path, GPoint(window_bounds.size.w/2, window_bounds.size.h/2 - 10));
-                                 
-  update_time();
+            
+  rotate_arrow(angle);
 }
 
 static void main_window_unload(Window *window) {
-  text_layer_destroy(time_layer);
-  text_layer_destroy(address_text_layer);
+  text_layer_destroy(timer_layer);
+  text_layer_destroy(cardinal_text_layer);
   text_layer_destroy(distance_text_layer);
-// bitmap_layer_destroy(direction_arrow);
   gpath_destroy(arrow_path);
   layer_destroy(path_layer);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-}
-
 void handle_init(void) {
-
-  // Create main Window
-  my_window = window_create();
-
-  time_layer = text_layer_create(GRect(0, 0, 144, 20));
   
-  window_set_window_handlers(my_window, (WindowHandlers) {
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
+  // Create Windows
+  main_window = window_create();
+  calibration_window = window_create();
+  
+  window_set_window_handlers(calibration_window, (WindowHandlers) {
+    .load = calibration_window_load,
+    .unload = calibration_window_unload
+  });  
+  
+  window_stack_push(calibration_window, true);
+  window_set_click_config_provider(calibration_window, config_provider);
+  
+  window_set_window_handlers(main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
-//  window_set_click_config_provider(my_window, click_config_provider);
   
-  window_stack_push(my_window, true);
-  window_set_click_config_provider(my_window, config_provider);
-  
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+//  window_set_click_config_provider(main_window, click_config_provider);
+  window_stack_push(main_window, true);
+  window_set_click_config_provider(main_window, config_provider);
   
 }
 
 void handle_deinit(void) {
-  window_destroy(my_window);
+  window_destroy(calibration_window);
+  window_destroy(main_window);
 }
 
 int main(void) {
+
   handle_init();
   app_event_loop();
   handle_deinit();
